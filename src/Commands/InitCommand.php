c93860f71e35f714fce79c06d31c6f20c35c9ba8
Ni Nelli <nellini@ronasit.com>
2025-04-21 14:15:14 +0900

    refactor: change renovate command default answer

--- src/Commands/InitCommand.php
+++ src/Commands/InitCommand.php
@@ -129,7 +129,7 @@ class InitCommand extends Command implements Isolatable
             }
         }
 
-        if ($this->confirm('Would you use Renovate dependabot?')) {
+        if ($this->confirm('Would you use Renovate dependabot?', true)) {
             $this->saveRenovateJSON();
 
             if ($shouldGenerateReadme) {
