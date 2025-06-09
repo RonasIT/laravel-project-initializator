fa2367e32718ffc87d07c0722e28747319af0846
Anton Zabolotnikov <110885041+AZabolotnikov@users.noreply.github.com>
2025-06-09 10:32:50 +0500

    Update tests/fixtures/InitCommandTest/add_clerk_id_to_users_table_migration.php

--- tests/fixtures/InitCommandTest/add_clerk_id_to_users_table_migration.php
+++ tests/fixtures/InitCommandTest/add_clerk_id_to_users_table_migration.php
@@ -19,7 +19,6 @@ class AddClerkIdToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropUnique('users_clerk_id_unique');
             $table->dropColumn('clerk_id');
         });
     }
