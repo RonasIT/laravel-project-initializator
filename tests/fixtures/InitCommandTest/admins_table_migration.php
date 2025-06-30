810e96f84be584f6ff805c0776e2188be5a0389e
DenTray <dpankratov@ronasit.com>
2025-06-30 09:48:38 +0600

    Apply suggestions from code review

--- tests/fixtures/InitCommandTest/admins_table_migration.php
+++ tests/fixtures/InitCommandTest/admins_table_migration.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
@@ -19,7 +20,7 @@ class AdminsCreateTable extends Migration
             $table->string('password');
         });
 
-        if (config('app.env') !== 'testing'){
+        if (!App::environment('testing')) {
             DB::table('admins')->insert([
                 'email' => 'mail@mail.com',
                 'password' => Hash::make('123456'),
