eca70eac0c0ab96d3ab491d2df56bdb631619ec2
Ruslan Guskov <feniksuh@gmail.com>
2025-08-27 13:59:26 +0300

    fix: fixture

--- tests/fixtures/InitCommandTest/user_model.php
+++ tests/fixtures/InitCommandTest/user_model.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
-    protected $fillable = ['name', 'email', 'password', 'clerk_id'];
+    protected $fillable = ['name', 'email', 'password'];
     /**
      * The attributes that should be hidden for serialization.
      *
