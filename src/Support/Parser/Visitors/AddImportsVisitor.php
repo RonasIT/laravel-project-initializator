0f70a2c2026f8b3b52a40b322a5ef92f35b74a7d
Ruslan Guskov <feniksuh@gmail.com>
2025-09-02 17:07:01 +0300

    refactor: addImportsVisitor

--- src/Support/Parser/Visitors/AddImportsVisitor.php
+++ src/Support/Parser/Visitors/AddImportsVisitor.php
@@ -17,13 +17,37 @@ class AddImportsVisitor extends NodeVisitorAbstract
     {
     }
 
-    public function enterNode(Node $node)
+    public function enterNode(Node $node): void
     {
         if (!$node instanceof Namespace_) {
-            return null;
+            return;
         }
 
+        $existingImports = $this->getExistingImports($node);
+
+        $importsToAdd = array_diff($this->classFullNames, $existingImports);
+
+        if (empty($importsToAdd)) {
+            return;
+        }
+
+        $newUseStmts = $this->getNewImports($importsToAdd);
+
+        $inserted = false;
+
+        $this->insertImportsToUseBlock($node, $newUseStmts, $inserted);
+
+        if (!$inserted) {
+            foreach ($newUseStmts as $useStmt) {
+                $node->stmts[] = $useStmt;
+            }
+        }
+    }
+
+    protected function getExistingImports(Node $node): array
+    {
         $existingUses = [];
+
         foreach ($node->stmts as $stmt) {
             if ($stmt instanceof Use_) {
                 foreach ($stmt->uses as $use) {
@@ -32,33 +56,32 @@ class AddImportsVisitor extends NodeVisitorAbstract
             }
         }
 
-        $toAdd = array_diff($this->classFullNames, $existingUses);
-        if (empty($toAdd)) {
-            return null;
-        }
+        return $existingUses;
+    }
 
+    protected function getNewImports(array $importsToAdd): array
+    {
         $newUseStmts = [];
-        foreach ($toAdd as $classFullName) {
+
+        foreach ($importsToAdd as $classFullName) {
             $newUseStmts[] = new Use_([
                 new UseUse(new Name($classFullName))
             ]);
         }
 
-        $inserted = false;
+        return $newUseStmts;
+    }
+
+    protected function insertImportsToUseBlock(Node $node, array $newUseStmts, bool &$inserted): void
+    {
         foreach ($node->stmts as $i => $stmt) {
             if (!($stmt instanceof Use_)) {
                 array_splice($node->stmts, $i, 0, $newUseStmts);
+
                 $inserted = true;
+
                 break;
             }
         }
-
-        if (!$inserted) {
-            foreach ($newUseStmts as $useStmt) {
-                $node->stmts[] = $useStmt;
-            }
-        }
-
-        return null;
     }
 }
