168773076a354194cc78068a7f1b9cabbdcb6eb3
Artyom Osepyan <art@osepyan.ru>
2025-12-23 11:46:52 +0300

    feat: add default HttpException render

--- stubs/default_http_exception.blade.php
+++
@@ -1,5 +0,0 @@
-$exceptions->render(function (HttpException $exception, Request $request) {
-    return ($request->expectsJson())
-        ? response()->json(['error' => $exception->getMessage()], $exception->getStatusCode())
-        : null;
-});
--- tests/fixtures/InitCommandTest/app_after_changes.php
+++ tests/fixtures/InitCommandTest/app_after_changes.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Http\Request;
-use Symfony\Component\HttpKernel\Exception\HttpException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -16,10 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->render(function (HttpException $exception, Request $request) {
-            return ($request->expectsJson())
-                ? response()->json(['error' => $exception->getMessage()], $exception->getStatusCode())
-                : null;
+        $exceptions->render(function (Symfony\Component\HttpKernel\Exception\HttpException $exception, Illuminate\Http\Request $request) {
+            return $request->expectsJson() ? response()->json(['error' => $exception->getMessage()], $exception->getStatusCode()) : null;
         });
-        //
     })->create();
